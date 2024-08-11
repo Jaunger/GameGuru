9329b288823a1a81fb50aea0d61820468eff28f9
Jaunger <121428460+Jaunger@users.noreply.github.com>
2024-08-11 21:00:26 +0300

    Login+Register

--- app/src/main/java/com/daniel/gameguru/Activity_Login.java
+++ app/src/main/java/com/daniel/gameguru/Activity_Login.java
@@ -6,16 +6,12 @@ import android.util.Log;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.AppCompatButton;
 import androidx.appcompat.widget.AppCompatTextView;
 
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputEditText;
-import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
